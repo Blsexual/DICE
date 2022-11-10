508885ce8df4bda81af7e26d1a63afab67b3e6e3
Erik <erik.gustafsson03@elev.ntig.se>
2022-11-10 10:17:14 +0100

    character basic

--- character.js
+++ character.js
@@ -1,14 +1,58 @@
 
-protag = document.getElementById('protag');
 addEventListener('keydown', move);
-speed = 10;
+let speed = 10;
+let x = 50;
+let y = 50;
+let width = 20;
+let height = 10;
 
+
+class character{
+    constructor(x, y, width, height, color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    drawCharacter(){
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+let protag = new character(x,y,width,height,"green");
+protag.drawCharacter()
 
 function move(e){
-    console.log(e)
+    console.log(canvas.width)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (e.key == "ArrowRight" || e.key == "d"){
-        console.log("hump")
-        protag 
+        protag.x += speed;   
     }
+    else if (e.key == "ArrowLeft" || e.key == "a"){
+        protag.x -= speed; 
+    }
+    else if (e.key == "ArrowUp" || e.key == "w"){
+        protag.y -= speed;   
+    }
+    else if (e.key == "ArrowDown" || e.key == "s"){
+        protag.y += speed;    
+    }
+    if (protag.x < 0){
+        protag.x = 0
+    }
+    if (protag.x > (canvas.width - width)){
+        protag.x = canvas.width - width
+    }
+    if (protag.y < 0){
+        protag.y = 0
+    }
+    if (protag.y > (canvas.height - height)){
+        protag.y = canvas.height - height
+    }
+    protag.drawCharacter()
 
 }
