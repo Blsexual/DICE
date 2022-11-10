2065b982aeba230f3e37c6302d9b53b4395e92b8
Erik <erik.gustafsson03@elev.ntig.se>
2022-11-10 13:56:27 +0100

    dt

--- canvas.js
+++ canvas.js
@@ -1,106 +1,21 @@
-//'use strict'
-//Kanvasen skapas
-var canvas = document.getElementById("Canvas"); 
-var ctx = canvas.getContext("2d");
+
+let btstart = new Image();
+btstart.src = "img/start.png";
+
+let hp = 50;
+
+let click = {x:null,y:null};
+
+let box1 = {x:0,y:0,width:771/4,height:264/4}; //? idk
+let box2 = {x:650,y:400,width:771/4,height:264/4}; //? idk
 
 
-//Creating the grid system for the map
-let grid_list = [];
-
-/*
-class Grid{
-    constructor(width, height, color){
-        this.width = width;
-        this.height = height;
-        this.color = color;
-    }
-}
-
-
-for(let i=0; i<5; i++){
-    let block = new Grid(250,250,"black");
-    grid_list_x.push(block);
-}
-
-
-
-
-function gridY(){
-    for(let i=0; i<3; i++){
-        ctx.beginPath();   
-        let block = new Grid(250,250,"black");
-        grid_list_y.push(block);
-        ctx.rect(0, 0, block.width, block.height);
-    } 
-}
-
-
-//creating the path for the player to move on the map grid
-class Path{
-    constructor(x, y, width, height, color){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = color;
-    }
-}
-*/
-
-
-
-
-// Sets important constants and variables
-
-const container = document.getElementById("container");
-
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  let x = 1;
-  let y = 1;
-  
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.setAttribute("id", y + ":" + x);
-    x++;
-    if(x > cols){
-        x = 1;
-        y++;
-    }
-    container.appendChild(cell).className = "grid-item";
+window.onload = function() {
+    var canvas = document.getElementById("Canvas");
+    canvas.addEventListener("mousedown", mousedown);
+    var ctx = canvas.getContext("2d"); 
+    ctx.drawImage(btstart,box1.x,box1.y,box1.width,box1.height);
   };
-};
-
-function Path(){
-    document.getElementById("4:1").style.backgroundColor = "red";
-    document.getElementById("4:1").className = "path";
-    document.getElementById("4:2").style.backgroundColor = "red";
-    document.getElementById("4:2").className = "path";
-    document.getElementById("4:3").style.backgroundColor = "red";
-    document.getElementById("4:3").className = "path";
-    document.getElementById("4:4").style.backgroundColor = "red";
-    document.getElementById("4:4").className = "path";
-    document.getElementById("4:5").style.backgroundColor = "red";
-    document.getElementById("4:5").className = "path";
-    document.getElementById("4:6").style.backgroundColor = "red";
-    document.getElementById("4:6").className = "path";
-    document.getElementById("4:7").style.backgroundColor = "red";
-    document.getElementById("4:7").className = "path";
-    document.getElementById("4:8").style.backgroundColor = "red";
-    document.getElementById("4:8").className = "path";
-    document.getElementById("4:9").style.backgroundColor = "red";
-    document.getElementById("4:9").className = "path";
-
-}
 
 
 
-function gameloop(){
-    makeRows(8, 18);
-    Path();
-}
-
-window.onload = function(){
-   window.requestAnimationFrame(gameloop);
-}
---
+++ click.js
@@ -0,0 +1,14 @@
+function mousedown(event){
+ 
+    click.x = event.clientX;
+    click.y = event.clientY;
+    console.log(click.x)
+
+    if ((click.x > box1.x) && (click.x < box1.x + box1.width)){
+        if ((click.y > box1.y) && (click.y < box1.y + box1.height)){         
+            hp -= 1;
+            console.log(hp);     
+        }
+    }
+
+}
