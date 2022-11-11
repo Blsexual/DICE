7f683a38c163a383d8e1e72c7b287c04e1b39bd6
Erik <erik.gustafsson03@elev.ntig.se>
2022-11-11 08:48:07 +0100

    enemy combat

--- canvas.js
+++ canvas.js
@@ -3,10 +3,13 @@ canvas.addEventListener("mousedown", mousedown);
 var ctx = canvas.getContext("2d");
 var block;
 let grid_list = [];
+
 let r=1;
 let g=1;
 let b=1;
 var test;
+let startPosition = new gridbox(100*(4-1),100*(7-1),"green",100,100);
+
 
 let btstart = new Image();
 btstart.src = "img/start.png";
@@ -22,27 +25,29 @@ let click = {x:null,y:null};
 let box1 = {x:0,y:0,width:771/4,height:264/4}; //? idk
 
 function init(){
-    for(let y=1; y<8; y++){
-        for(let x=1; x<16; x++){
-            r+=1;
-            g+=1;
-            b+=1;
-            block = new gridbox(100*(x-1),100*(y-1),"rgb(0"+r+",0"+g+",0"+b+")",100,100);
+    for(let y=0; y<8; y++){
+        for(let x=0; x<15; x++){
+            block = new gridbox(100*(x-1),100*(y-1),"red",100,100);
             grid_list.push(block);
         } 
     }
     test = new enemy(10,1,5,head,300,300,50,50)
 }
+
 function gameloop(){
     draw();
     console.log(grid_list);
+    console.log(startPosition);
 }
 
 
 function draw(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    startPosition.draw(ctx);
     grid_list.forEach(gridblock => {
-        gridblock.draw(ctx);
+       gridblock.draw(ctx); 
     });
+    
     ctx.drawImage(btstart,box1.x,box1.y,box1.width,box1.height);
 
 
